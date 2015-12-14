06e216ca62a0e5955e2abcbdc5ca3175e822de87
Sergey Parshukov <codedby@bugman.me>
2015-12-14 17:15:59 +0300

    Nicer glow

--- example/main.go
+++ example/main.go
@@ -9,17 +9,19 @@ import (
 )
 
 func glowLEDs(move psmove.Move) {
-	gr := perlin.NewGenerator(2, 2, 1, 100)
-	gg := perlin.NewGenerator(2, 2, 1, 200)
-	gb := perlin.NewGenerator(2, 2, 1, 300)
+	gr := perlin.NewGenerator(2, 2, 3, 100)
+	gg := perlin.NewGenerator(2, 2, 3, 200)
+	gb := perlin.NewGenerator(2, 2, 3, 300)
 
 	f := func(g *perlin.Generator, t float64) int {
 		return int(254.0 * (g.Noise1D(t) + 1.0) / 2.0)
 	}
 	t := 0.0
 	for {
-		t += 0.05
-		move.SetLEDs(f(gr, t), f(gg, t), f(gb, t))
+		t += 0.03
+		r, g, b := f(gr, t), f(gg, t), f(gb, t)
+		// log.Println(r, g, b)
+		move.SetLEDs(r, g, b)
 		move.UpdateLEDs()
 		time.Sleep(15 * time.Millisecond)
 	}
