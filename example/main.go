5d49387f3f8732b5f91ea60020d4666c7aba82ee
Sergey Parshukov <codedby@bugman.me>
2015-12-15 14:00:25 +0300

    Buttons and extended example

--- example/main.go
+++ example/main.go
@@ -13,19 +13,24 @@ func glowLEDs(move psmove.Move) {
 	gg := perlin.NewGenerator(2, 2, 3, 200)
 	gb := perlin.NewGenerator(2, 2, 3, 300)
 
-	f := func(g *perlin.Generator, t float64) int {
-		return int(254.0 * (g.Noise1D(t) + 1.0) / 2.0)
+	f := func(g *perlin.Generator, t float64) byte {
+		return byte(254.0 * (g.Noise1D(t) + 1.0) / 2.0)
 	}
 	t := 0.0
 	for {
 		t += 0.03
 		r, g, b := f(gr, t), f(gg, t), f(gb, t)
 		// log.Println(r, g, b)
+		if force != 0 {
+			continue
+		}
 		move.SetLEDs(r, g, b)
 		move.UpdateLEDs()
 		time.Sleep(15 * time.Millisecond)
 	}
 }
+
+var force byte
 
 func main() {
 	move, err := psmove.Connect()
@@ -33,6 +38,15 @@ func main() {
 		log.Fatal(err)
 	}
 	defer move.Disconnect()
-	log.Println(move.ConnectionType())
-	glowLEDs(move)
+	go glowLEDs(move)
+	for {
+		if move.ConnectionType() == psmove.ConnectionTypeBluetooth && move.Poll() {
+			force = move.GetTriggerValue()
+			move.SetRumble(force)
+			if force > 0 {
+				move.SetLEDs(force, 0, 0)
+			}
+			move.UpdateLEDs()
+		}
+	}
 }
--- psmove.go
+++ psmove.go
@@ -26,21 +26,70 @@ func (move Move) Disconnect() {
 	C.psmove_disconnect(move.pointer)
 }
 
-func (move Move) ConnectionType() string {
+type ConnectionType uint
+
+const (
+	ConnectionTypeBluetooth ConnectionType = iota
+	ConnectionTypeUSB
+	ConnectionTypeUnknown
+)
+
+func (move Move) ConnectionType() ConnectionType {
 	switch C.psmove_connection_type(move.pointer) {
 	case C.Conn_Bluetooth:
-		return "Bluetooth"
+		return ConnectionTypeBluetooth
 	case C.Conn_USB:
-		return "USB"
+		return ConnectionTypeUSB
 	default:
-		return "unknown"
+		return ConnectionTypeUnknown
 	}
 }
 
-func (move Move) SetLEDs(r, g, b int) {
+func (move Move) SetLEDs(r, g, b byte) {
 	C.psmove_set_leds(move.pointer, C.uchar(r), C.uchar(g), C.uchar(b))
+}
+
+func (move Move) SetRumble(value byte) {
+	C.psmove_set_rumble(move.pointer, C.uchar(value))
 }
 
 func (move Move) UpdateLEDs() {
 	C.psmove_update_leds(move.pointer)
+}
+
+func (move Move) Poll() bool {
+	return C.psmove_poll(move.pointer) > 0
+}
+
+func (move Move) getButtons() C.uint {
+	bits := C.psmove_get_buttons(move.pointer)
+	return bits
+}
+
+func (move Move) IsTriggerPressed() bool {
+	return (move.getButtons() & C.Btn_T) != 0
+}
+
+func (move Move) IsSquarePressed() bool {
+	return (move.getButtons() & C.Btn_SQUARE) != 0
+}
+
+func (move Move) IsTrianglePressed() bool {
+	return (move.getButtons() & C.Btn_TRIANGLE) != 0
+}
+
+func (move Move) IsCrossPressed() bool {
+	return (move.getButtons() & C.Btn_CROSS) != 0
+}
+
+func (move Move) IsCirclePressed() bool {
+	return (move.getButtons() & C.Btn_CIRCLE) != 0
+}
+
+func (move Move) IsPSButtonPressed() bool {
+	return (move.getButtons() & C.Btn_PS) != 0
+}
+
+func (move Move) GetTriggerValue() byte {
+	return byte(C.psmove_get_trigger(move.pointer))
 }
